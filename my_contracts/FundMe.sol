9aa09013222e4e76a884bd09cf5e78762411340a
Yu-B-B <xiafan98@163.com>
2025-08-23 17:55:39 +0800

    refount

--- my_contracts/FundMe.sol
+++ my_contracts/FundMe.sol
@@ -12,10 +12,14 @@ contract FundMe{
 
     mapping (address => uint256) internal funderMap;
 
-    // eth 最小单位
-    uint256 MINIMUM_VALUE = 1 * 10 ** 10;
+    // eth 最小单位,设置为常量，任何位置都无法改变
+    uint256 constant MINIMUM_VALUE = 1 * 10 ** 10;
     // usd 最小单位，100USD
-    uint256 MINIMUM_USD_VALUE = 100 * 10 ** 18;
+    uint256 constant MINIMUM_USD_VALUE = 100 * 10 ** 18;
+
+    // 替换阈值
+    // uint256 MAXMUM_VALUE = 1000 * 10 ** 18; //UDS
+    uint256 MAXMUM_VALUE = 4 * 10 ** 10; //UDS
 
     AggregatorV3Interface internal dataFeed;
 
@@ -29,7 +33,8 @@ contract FundMe{
 
     // 1、收款函数，address uint
     function fund() external payable {
-        require(calculateUsdPrice(msg.value) < MINIMUM_USD_VALUE, "it's too "); // 如果使用链上数量判断，不符合实际使用者体感
+        require(msg.value < MINIMUM_USD_VALUE, "it's too "); // 如果使用链上数量判断，不符合实际使用者体感
+        // require(calculateUsdPrice(msg.value) < MINIMUM_USD_VALUE, "it's too "); // 如果使用链上数量判断，不符合实际使用者体感
         funderMap[msg.sender] = msg.value;
     }
     
@@ -38,10 +43,39 @@ contract FundMe{
         return funderMap[_user];
     }
 
+    // 提款
+    function getFund() external isOwner{
+        // address(this) 返回当前合约
+        require(address(this).balance >= MAXMUM_VALUE, "target is not reached");
+        // require(calculateUsdPrice(address(this).balance) >= MAXMUM_VALUE, "target is not reached");
+        // 将合约中balance转移给owner,转账方式：transfer（只有转账操作）、send）、call（任何情况下都可使用，纯转账、转账中存在其他操作）
+        // transfer，将eth发送到另一个地址，失败将返还
+        payable(msg.sender).transfer(address(this).balance);
+        // sender，最终还是transfer,成功放回true，失败返回false
+        // call，
+    }
+
+    // 退款
+    function refund() external {
+        // 已经筹满时，不允许退款
+        require(address(this).balance < MAXMUM_VALUE, "target is reached");
+        // 为所有捐款人退款
+        uint256 amount = found(msg.sender);
+        require(amount != 0,"there is no fund for you");
+        bool success;
+        (success,) = payable(msg.sender).call{value: amount}("");
+    }
+
     //
     function foundContractUser() public view returns(address) {
         return owner;
     }
+
+    // 合约所有权转移
+    function transferOwnerShip(address _newOwner) public  isOwner{
+        owner = _newOwner;
+    }
+
 
     // chain link data feed
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
@@ -61,6 +95,11 @@ contract FundMe{
         uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
         // 确保交易对的两个参数精度一致，_amount 是eth的精度，ethPrice是USD精度
         return _amount * ethPrice / (10 ** 8);
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "you don't have permission");
+        _;
     }
 
 }
