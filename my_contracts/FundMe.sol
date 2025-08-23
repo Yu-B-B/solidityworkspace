4c7e613907cd2c7b9052448e7087219a2fa73c76
Yu-B-B <xiafan98@163.com>
2025-08-23 15:26:18 +0800

    add data feed

--- my_contracts/FundMe.sol
+++ my_contracts/FundMe.sol
@@ -12,7 +12,10 @@ contract FundMe{
 
     mapping (address => uint256) internal funderMap;
 
+    // eth 最小单位
     uint256 MINIMUM_VALUE = 1 * 10 ** 10;
+    // usd 最小单位，100USD
+    uint256 MINIMUM_USD_VALUE = 100 * 10 ** 18;
 
     AggregatorV3Interface internal dataFeed;
 
@@ -20,12 +23,13 @@ contract FundMe{
 
     constructor(){
         owner = msg.sender;
-        // 初始化dataFeed，一次初始化后续将不再new。注意：调用三方合约时，需要确保合约部署到链上（测试），才能进行调用
+        // 初始化dataFeed，一次初始化后续将不再new。注意：调用三方合约时，需要确保合约部署到链上（测试），才能进行调用, ！！这里的地址为线上地址
+        dataFeed = AggregatorV3Interface(owner);
     } 
 
     // 1、收款函数，address uint
     function fund() external payable {
-        require(msg.value < MINIMUM_VALUE, "it's too "); // 如果使用链上数量判断，不符合实际使用者体感
+        require(calculateUsdPrice(msg.value) < MINIMUM_USD_VALUE, "it's too "); // 如果使用链上数量判断，不符合实际使用者体感
         funderMap[msg.sender] = msg.value;
     }
     
@@ -50,6 +54,13 @@ contract FundMe{
             /*uint80 answeredInRound*/
         ) = dataFeed.latestRoundData();
         return answer;
+    }
+
+    // calculate usd price use eth price and eth amount
+    function calculateUsdPrice(uint256 _amount) internal view returns(uint256){
+        uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
+        // 确保交易对的两个参数精度一致，_amount 是eth的精度，ethPrice是USD精度
+        return _amount * ethPrice / (10 ** 8);
     }
 
 }
